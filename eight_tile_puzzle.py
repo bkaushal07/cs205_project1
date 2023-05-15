54af99c76c89b8b883242ca84638f0d0006f5d86
Kaushal B <36352020+bkaushal07@users.noreply.github.com>
2023-05-15 12:09:17 -0700

    Updated the code with comments

--- eight_tile_puzzle.py
+++ eight_tile_puzzle.py
@@ -1,38 +1,49 @@
-import copy, time
+import time
+import copy
+
+# Our targetted goal state
 goal_state= [[1,2,3],[4,5,6],[7,8,0]]
 
+# Represents a node in a search algorithm and stores the problem/puzzle
 class Node:
     def __init__(self, problem, depth = 0, cost = 0):
-        self.problem = problem
-        self.depth = depth
-        self.cost = cost
+        self.problem = problem # The problem state associated with the node
+        self.depth = depth # The depth or level of the node in the search tree. Default is 0
+        self.cost = cost # The cost associated with the node. Default is 0
 
+
+# Generic Search algorithm
 def generic_search(root: Node, algorithm):
-    nodes = [root]
-    max_depth = 0 
-    expanded_nodes = 0 
-    
+    nodes = [root] # nodes = MAKE-QUEUE(MAKE-NODE(problem.INITIAL-STATE))
+    max_depth = 0 # to track deepest level
+    expanded_nodes = 0 # count of nodes that have been expanded or visited
     while nodes:
         node = nodes.pop(0) # popping the first element in the list
         # print(node)
         # print(node.problem)
-        if node.problem == goal_state:
+        if node.problem == goal_state: # check if at goal state
             print(f'Hooray!!! We did it in {expanded_nodes} steps')
             return node
-        nodes = queueing_function(node, nodes, algorithm)        
+        nodes = queueing_function(node, nodes, algorithm) # nodes is updated with the newly generated nodes as per chosen search algorithm
         expanded_nodes+=1
         max_depth = max(max_depth, node.depth)
     print('NADA!!')
-    return None
+    return None # return None if no solution
 
+
+
+# Calculates the number of misplaced tiles in the given puzzle state compared to the goal state.
 def heuristic_misplaced_tile(root: Node):
     misplaced_count = 0
     for i in range(len(root.problem)):
         for j in range(len(root.problem[i])):
             if goal_state[i][j] != root.problem[i][j] and all(v != len(root.problem[i]) for v in (i, j)):
+                # Count the number of misplaced tiles in the root problem compared to the goal state
                 misplaced_count += 1
-    return misplaced_count
+    return misplaced_count # return the count of misplaced tiles
 
+
+# Calculates the Manhattan distance heuristic for the given puzzle state compared to the goal state.
 def heuristic_manhattan_distance(root: Node):
     total_distance = 0
     for i in range(len(root.problem)):
@@ -40,13 +51,15 @@ def heuristic_manhattan_distance(root: Node):
             x, y = is_possible(goal_state, root.problem[i][j])
             if root.problem[i][j] != 0:
                 total_distance += abs(x - i) + abs(y - j)
-    return total_distance
+    return total_distance # The total Manhattan distance
+
 
 def queueing_function(node, nodes, algorithm):
     global visited_states # to keep track of the visited states 
-    a=copy.deepcopy(node)
+    a = copy.deepcopy(node)
     # print(a)
 
+    # Update the depth and cost of the node a based on the selected algorithm
     def update_node_cost(a, algorithm):
         a.depth = node.depth + 1
         if algorithm == '1':
@@ -59,12 +72,19 @@ def queueing_function(node, nodes, algorithm):
             # print("in manhattan")
             a.cost = a.depth + heuristic_manhattan_distance(a)
     
+    # Enqueue the node a into the list of nodes if it has not been visited before
     def enqueue_node(a):
         if [a.cost, a.problem] not in visited_states:
             nodes.append(a)
             # print(nodes)
             visited_states.append([a.cost, a.problem])
             # print(visited_states)
+    
+    
+    # Generate successor nodes by applying the up, down, left, and right operators 
+    # to the current node node.
+    # If a successor node is valid (not None), update its cost based on the selected algorithm 
+    # and enqueue it into the list of nodes.
 
     a = Operators.go_up(copy.deepcopy(node))
     if a is not None:
@@ -86,14 +106,22 @@ def queueing_function(node, nodes, algorithm):
         update_node_cost(a, algorithm)
         enqueue_node(a)
 
-    nodes.sort(key = lambda a:(a.cost,a.depth))
-    print ('\n***Searching at depth', str(nodes[0].depth),'\n')
+    nodes.sort(key = lambda a:(a.cost,a.depth)) # Sort the list of nodes based on their cost and depth
+    print ('\n***Searching at depth', str(nodes[0].depth),'\n') # Depth of the node with the lowest cost
     display_state(nodes[0])
     return nodes
+
+
+
+# Methods for moving the blank space (0) in different directions,
+# swaps the blank space with the adjacent number in the specified direction 
+# and returns the updated node. Else, return None indicating 
+# that the move is not possible in that direction.
 
 class Operators():
     def go_up(root: Node):
         i, j = is_possible(root.problem, 0)
+        # checks if the value of i is not equal to 2, which means the blank space is not in the bottom row of the puzzle. 
         if i != 2:
             root.problem[i][j], root.problem[i+1][j] = root.problem[i+1][j], root.problem[i][j]
             return root
@@ -110,6 +138,7 @@ class Operators():
 
     def go_left(root: Node):
         i, j = is_possible(root.problem, 0)
+        # checks if the value of j is not equal to 2, which means the blank space is not in the rightmost column of the puzzle.
         if j != 2:
             root.problem[i][j], root.problem[i][j+1] = root.problem[i][j+1], root.problem[i][j]
             return root
@@ -124,7 +153,9 @@ class Operators():
         else:
             return None
 
-# checks and returns coordinates if the numbers match with the any state of problem 
+
+# checks if a given value is present in a 2D problem array and 
+# returns its coordinates (row, column) if found
 def is_possible(problem, value):
     for i in range(len(problem)):
         for j in range(len(problem[i])):
@@ -132,6 +163,7 @@ def is_possible(problem, value):
                 return i, j
     return -1, -1 # returns -1 if the problem match fails 
 
+# Display the state of a node by printing its problem array
 def display_state(node: Node):
     for i in node.problem:
         for j in i:
@@ -141,6 +173,8 @@ def display_state(node: Node):
             print(j, end=' ')
         print()
 
+
+# A menu to the user for selecting options related to the 8-tile puzzle
 def menu():
     test_cases = {
                     '1':[[1,2,3],[4,5,6],[7,8,0]],
